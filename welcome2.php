2c5497d059f371043ddf1085ed5d7647e450e9a8
CoderArpan <140335451+CoderArpan@users.noreply.github.com>
2023-07-23 21:36:19 +0530

    Add files via upload

---
+++ welcome2.php
@@ -0,0 +1,354 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="style.css">
+  <title>Inspire Coaching academy</title>
+  <link rel="icon" type="image/x-icon" href="favicon.jpg">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body  id="body">
+  
+  <div id="myDIV">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" id="myNav">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="logo-removebg-preview.png"
+            style="padding-left: 10px; max-width: 250px;" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="stripe.html">Payment</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Faculty</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Why us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">career</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./account2.php">Account</a>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link" href="./register.php">Change</a>
+            </li> -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Courses
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">CHEMISTRY</a></li>
+                <li><a class="dropdown-item" href="#">BIOLOGY</a></li>
+                <li><a class="dropdown-item" href="#">PYHSICS</a></li>
+                <li><a class="dropdown-item" href="#">MATHS</a></li>
+                <li><a class="dropdown-item" href="#">COMPUTER</a></li>
+                <li><a class="dropdown-item" href="#">ENGLISH</a></li>
+                <li><a class="dropdown-item" href="#">SOCIAL SCIENCE</a></li>
+                <li><a class="dropdown-item" href="#">BENGALI</a></li>
+                <li><a class="dropdown-item" href="#">HINDI</a></li>
+                <li><a class="dropdown-item" href="#">BENGALI</a></li>
+                <li><a class="dropdown-item" href="#">JEE</a></li>
+                <li><a class="dropdown-item" href="#">NEET</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Student Zone
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Change theme
+              </a>
+              <ul class="dropdown-menu">
+                <li><button id="btn2">lite</button></li>
+                <li><button id="btn">dark</button></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+    </nav>
+  </div>
+  <div class="bg-secondary py-3 text-center">
+    <a href="#" class="bnt- text-white  fw-bold py-3 px-4">Apply For Online classes</a>
+
+  </div>
+  <!-- owl corsol -->
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="display: block;">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="10000 ">
+        <img
+          src="carsoul1.jpg"
+          class="d-block w-100 " style="max-height: 600px; padding: 90px;" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img
+          src="carsoule2.jpg"
+          class="d-block w-100" style="max-height: 600px; padding: 90px;" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img
+          src="carsoul3.jpg"
+          class="d-block w-100" style="max-height: 600px; padding: 90px;" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img
+          src="carsoul4.jpg"
+          class="d-block w-100" style="max-height: 600px; padding: 90px;" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <!-- ce -->
+  
+  <h1 style="padding: 90px;">WHY ICA??</h1>
+  <div class="iframe-container">
+    <center><iframe width="700" height="600" style="padding-bottom: 50px;" src="https://www.youtube.com/embed/7fRRlW1TuS4" title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen></iframe></center>
+  </div>
+
+  <!-- Side by side image -->
+  <div class="container">
+    <div class="row align-items-start">
+      <div class="col">
+        <p>
+          A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors
+          116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in
+          some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.
+        </p>
+        <button type="button" class="btn btn-warning">Read more &#8919;</p> </button>
+      </div>
+      <div class="col-md-6">
+        <img
+          src="https://images.unsplash.com/photo-1688168293343-e1c824a4ace5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          style="max-width: 320px;" alt="">
+      </div>
+
+
+
+
+    </div>
+
+  </div>
+ 
+
+
+
+<!-- footer -->
+<div class="footer-clean" id="myFooter">
+  <footer>
+      <div class="container">
+          <div class="row justify-content-center">
+              <div class="col-sm-4 col-md-3 item">
+                  <h3>Services</h3>
+                  <ul>
+                      <li><a href="#">........</a></li>
+                      <li><a href="#">........</a></li>
+                      <li><a href="#">........</a></li>
+                  </ul>
+              </div>
+              <div class="col-sm-4 col-md-3 item">
+                  <h3>About</h3>
+                  <ul>
+                      <li><a href="#">........</a></li>
+                      <li><a href="#">........</a></li>
+                      <li><a href="#">........</a></li>
+                  </ul>
+              </div>
+              <div class="col-sm-4 col-md-3 item">
+                  <h3>Careers</h3>
+                  <ul>
+                      <li><a href="#">........</a></li>
+                      <li><a href="#">........</a></li>
+                      <li><a href="#">.......</a></li>
+                  </ul>
+              </div>
+              <div class="col-lg-3 item social"><a href="https://www.facebook.com/soumya.mukherjii/"><i class="icon ion-social-facebook"></i></a><a href=""><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                  <p class="copyright">INSPIRE COACHING ACADEMY © 2023</p>
+              </div>
+          </div>
+      </div>
+  </footer>
+<!-- footer -->
+
+
+</body>
+<!-- style sections -->
+<style>
+  body{
+    background-image: url(user\ image-lite.jpg);
+    color: red;
+  }
+  .iframe-container {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-bottom: 56.25%; /* 16:9 aspect ratio (change as needed) */
+  }
+  .iframe-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .footer-clean {
+padding:50px 0;
+background-color:white;
+color:black;
+}
+
+.footer-clean h3 {
+margin-top:0;
+margin-bottom:12px;
+font-weight:bold;
+font-size:16px;
+}
+
+.footer-clean ul {
+padding:0;
+list-style:none;
+line-height:1.6;
+font-size:14px;
+margin-bottom:0;
+}
+
+.footer-clean ul a {
+color:inherit;
+text-decoration:none;
+opacity:0.8;
+}
+
+.footer-clean ul a:hover {
+opacity:1;
+}
+
+.footer-clean .item.social {
+text-align:right;
+}
+
+@media (max-width:767px) {
+.footer-clean .item {
+  text-align:center;
+  padding-bottom:20px;
+}
+}
+
+@media (max-width: 768px) {
+.footer-clean .item.social {
+  text-align:center;
+}
+}
+
+.footer-clean .item.social > a {
+font-size:24px;
+width:40px;
+height:40px;
+line-height:40px;
+display:inline-block;
+text-align:center;
+border-radius:50%;
+border:1px solid #ccc;
+margin-left:10px;
+margin-top:22px;
+color:inherit;
+opacity:0.75;
+}
+
+.footer-clean .item.social > a:hover {
+opacity:0.9;
+}
+
+@media (max-width:991px) {
+.footer-clean .item.social > a {
+  margin-top:40px;
+}
+}
+
+@media (max-width:767px) {
+.footer-clean .item.social > a {
+  margin-top:10px;
+}
+}
+
+.footer-clean .copyright {
+margin-top:14px;
+margin-bottom:0;
+font-size:13px;
+opacity:0.6;
+}
+</style>
+<!-- script links-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+<!-- script links-->
+
+<!-- script 2  -->
+<script>
+  // Get a reference to the button elements
+  var btnDark = document.getElementById("btn");
+  var btnLite = document.getElementById("btn2");
+
+  // Add event listeners to the buttons
+  btnDark.addEventListener("click", function () {
+    document.getElementById("body").style.backgroundImage = 'url("user\ image-dark.jpg")';
+    document.getElementById("body").style.color = "white";
+    document.getElementById("myFooter").style.backgroundColor = "black";
+    document.getElementById("myFooter").style.color = "white";
+  });
+
+  btnLite.addEventListener("click", function () {
+    document.getElementById("body").style.backgroundImage = 'url("user\ image-lite.jpg")';
+    document.getElementById("body").style.color = "red";
+    document.getElementById("myFooter").style.backgroundColor = "white";
+    document.getElementById("myFooter").style.color = "black";
+   
+  });
+</script>
+<!-- script 2 -->
+
+
+</html>
+
+
